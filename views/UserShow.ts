d65ca476b838ffbbdcf5a4dc11e618fed2e46c9d
BJ Jackson <smthbrothrjdev@gmail.com>
2022-10-31 23:02:11 -0500

    fixed formattinf of UserShow

--- views/UserShow.ts
+++ views/UserShow.ts
@@ -4,9 +4,12 @@ import { View } from "./View";
 export class UserShow extends View<User, UserProps> {
   template(): string {
     return `
-        <h1> User Detail</h1>
-        <h3> ${this.model.get("name")}</h3>
-        <h3> ${this.model.get("age")}
+       <div class="user-show">
+        <h3> name: ${this.model.get("name")}</h3>
+        <h3> age:  ${this.model.get("age")}
+        </div>
+       <br/>
+    
         `;
   }
 }
