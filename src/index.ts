267b799e042f65305728d7da7ee5a181bb2b1fc7
Action Jackson <smthbrothrjdev@gmail.com>
2022-11-01 09:49:14 -0500

    staged implementation for demo

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
-import { Collection } from "../models/Collection";
-import { UserList } from "../views/UserList";
-import { User, UserProps } from "../models/User";
+import { Collection } from '../models/Collection';
+import { UserList } from '../views/UserList';
+import { User, UserProps } from '../models/User';
+import { UserEdit } from '../views/UserEdit';
 
-const users = new Collection(
-  "http://localhost:3000/users",
-  (json: UserProps) => {
-    return User.buildUser(json);
-  }
-);
-
-users.on("change", () => {
-  const root = document.getElementById("root");
-  if (root) {
-    new UserList(root, users).render();
-  }
-});
-users.fetch();
+new UserEdit(
+  document.getElementById('root')!,
+  User.buildUser({ name: 'BJ', age: 70 })
+).render();
--- views/UserEdit.ts
+++ views/UserEdit.ts
@@ -1,22 +1,37 @@
-import { User, UserProps } from "../models/User";
-import { View } from "./View";
-import { UserForm } from "./UserForm";
-import { UserShow } from "./UserShow";
+import { User, UserProps } from '../models/User';
+import { View } from './View';
+import { UserForm } from './UserForm';
+import { UserShow } from './UserShow';
+import { UserList } from './UserList';
+import { Collection } from '../models/Collection';
 
 export class UserEdit extends View<User, UserProps> {
   //overide nested views
 
   regionsMap(): { [keys: string]: string } {
     return {
-      userShow: ".user-show",
-      userForm: ".user-form",
+      userShow: '.user-show',
+      userForm: '.user-form',
+      userList: '.user-list',
     };
   }
 
   //todo automate this
   onRender(): void {
+    //for collection
+    let users = User.buildCollection();
+    //add change method needed for collectionview as well as the fetch below
+    users.on('change', () => {
+      const root = this.regions.userList;
+      if (root) {
+        new UserList(root, users).render();
+      }
+    });
+    users.fetch();
+
     new UserShow(this.regions.userShow, this.model).render();
     new UserForm(this.regions.userForm, this.model).render();
+    new UserList(this.regions.userList, User.buildCollection()).render();
   }
 
   template(): string {
@@ -24,6 +39,7 @@ export class UserEdit extends View<User, UserProps> {
         <div>
             <div class="user-show"></div>
             <div class="user-form"></div>
+            <div class="user-list"></div>
         </div>
         `;
   }
