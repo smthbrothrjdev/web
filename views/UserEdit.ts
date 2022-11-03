0cb38c87525388a6216f51072e27991c4f34c7d7
Action Jackson <smthbrothrjdev@gmail.com>
2022-11-02 23:41:43 -0500

    centered items

--- views/UserEdit.ts
+++ views/UserEdit.ts
@@ -1,18 +1,18 @@
-import { User, UserProps } from "../models/User";
-import { View } from "./View";
-import { UserForm } from "./UserForm";
-import { UserShow } from "./UserShow";
-import { UserList } from "./UserList";
-import { Collection } from "../models/Collection";
+import { User, UserProps } from '../models/User';
+import { View } from './View';
+import { UserForm } from './UserForm';
+import { UserShow } from './UserShow';
+import { UserList } from './UserList';
+import { Collection } from '../models/Collection';
 
 export class UserEdit extends View<User, UserProps> {
   //overide nested views
 
   regionsMap(): { [keys: string]: string } {
     return {
-      userShow: ".user-show",
-      userForm: ".user-form",
-      userList: ".user-list",
+      userShow: '.user-show',
+      userForm: '.user-form',
+      userList: '.user-list',
     };
   }
 
@@ -21,7 +21,7 @@ export class UserEdit extends View<User, UserProps> {
     //for collection
     let users = User.buildCollection();
     //add change method needed for collectionview as well as the fetch below
-    users.on("change", () => {
+    users.on('change', () => {
       const parentElement = this.regions.userList;
       if (parentElement) {
         new UserList(parentElement, users).render();
@@ -37,11 +37,11 @@ export class UserEdit extends View<User, UserProps> {
   template(): string {
     return `
         <div class="row">
-            <div class="user-show col-md-8 offset-md-5"></div>
-            <div class="p-4 mr-4 user-form col-md-8 offset-md-4"></div>
+            <div class="user-show d-flex justify-content-center mt-3"></div>
+            <div class="user-form d-flex justify-content-center mt-3 mb-2 p-3" ></div>
            
             <hr />
-            <div class="user-list col-md-8 offset-md-5"></div>
+            <div class="user-list d-flex align-items-center flex-column"></div>
         </div>
         `;
   }
--- views/UserShow.ts
+++ views/UserShow.ts
@@ -4,7 +4,7 @@ import { View } from "./View";
 export class UserShow extends View<User, UserProps> {
   template(): string {
     return `
-    <div  class='bg-secondary bg-gradient m-1 card text-center' style="width: 18rem; --bs-bg-opacity: .3; " >
+    <div  class='bg-secondary bg-gradient m-1 card text-center' style="width: 18rem; --bs-bg-opacity: .3;" >
        <div class="user-show card-body">
         <h3> name: ${this.model.get("name")}</h3>
         <h3> age:  ${this.model.get("age")}
